8585c0c4eead89dae63946f9569527a344d3f188
Anurag Sharma <anuragsh868@gmail.com>
2026-02-01 21:06:17 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
+import * as _ from 'lodash';
+
+export const handler: Handler = async (event:APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+    // TODO implement
+    const max = 10;
+    const val = _.random(max);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(`The random number from max ${max} is ${val}`),
+    };
+    return response;
+  };
+  
