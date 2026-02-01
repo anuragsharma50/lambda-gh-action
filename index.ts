ae512e90f808867b286fa354ee332d76abf25808
Anurag Sharma <anuragsh868@gmail.com>
2026-02-02 02:03:12 +0530

    change max value

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
 
 export const handler: Handler = async (event:APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     // TODO implement
-    const max = 10;
+    const max = 100;
     const val = _.random(max);
     const response = {
       statusCode: 200,
